130cd744f7354f9b971e50852b5e2f47a8f1e7fa
m18332 <m18332@g.metro-cit.ac.jp>
2023-01-20 19:36:58 +0900

    1/20 commit

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4437: 遅すぎ ver.2")
+        return c.String(http.StatusOK, "4437: 遅すぎ ver.3")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-	    return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4437", "message": "遅すぎ ver.2"})
+	    return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4437", "message": "遅すぎ ver.3"})
     }
 }
